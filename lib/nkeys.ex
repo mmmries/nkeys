99e8b4e854f1ff496d83eed3964c3ef95f0d7fcf
mmmries <riesmmm@gmail.com>
2020-06-30 20:57:51 -0600

    initial

---
+++ lib/nkeys.ex
@@ -0,0 +1,18 @@
+defmodule Nkeys do
+  @moduledoc """
+  Documentation for `Nkeys`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Nkeys.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/nkeys_test.exs
@@ -0,0 +1,8 @@
+defmodule NkeysTest do
+  use ExUnit.Case
+  doctest Nkeys
+
+  test "greets the world" do
+    assert Nkeys.hello() == :world
+  end
+end
