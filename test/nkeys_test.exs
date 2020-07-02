68d70c111473d0f939b2e1eb275bfb5b1d222a6d
mmmries <riesmmm@gmail.com>
2020-07-01 23:03:03 -0600

    add seed validation

--- test/nkeys_test.exs
+++ test/nkeys_test.exs
@@ -1,26 +1,76 @@
 defmodule NKEYSTest do
   use ExUnit.Case
 
-  @seed "SUAMLK2ZNL35WSMW37E7UD4VZ7ELPKW7DHC3BWBSD2GCZ7IUQQXZIORRBU"
+  describe "from_seed/1" do
+    test "creates a struct from a valid seed" do
+      assert {:ok, nkey} = NKEYS.from_seed("SUAMLK2ZNL35WSMW37E7UD4VZ7ELPKW7DHC3BWBSD2GCZ7IUQQXZIORRBU")
+      assert nkey.private_key != nil
+      assert nkey.public_key != nil
+    end
 
-  test "from_seed" do
-    nonce = "PXoWU7zWAMt75FY"
-    {:ok, nkeys} = NKEYS.from_seed(@seed)
-    signed_nonce = NKEYS.sign(nkeys, nonce)
-    encoded_signed_nonce = Base.encode64(signed_nonce)
+    test "should raise error when seed has bad padding" do
+      assert {:error, :invalid_seed} = NKEYS.from_seed("UAMLK2ZNL35WSMW37E7UD4VZ7ELPKW7DHC3BWBSD2GCZ7IUQQXZIORRBU")
+    end
 
-    assert encoded_signed_nonce ==
-             "ZaAiVDgB5CeYoXoQ7cBCmq+ZllzUnGUoDVb8C7PilWvCs8XKfUchAUhz2P4BYAF++Dg3w05CqyQFRDiGL6LrDw=="
+    test "should raise error with invalid seeds" do
+      assert {:error, :invalid_seed} = NKEYS.from_seed("AUAMLK2ZNL35WSMW37E7UD4VZ7ELPKW7DHC3BWBSD2GCZ7IUQQXZIORRBU")
+
+      assert {:error, :invalid_seed} = NKEYS.from_seed("")
+
+      assert {:error, :invalid_seed} = NKEYS.from_seed(" ")
+    end
+
+    test "should validate prefix bytes" do
+      seeds = [
+               "SNAMLK2ZNL35WSMW37E7UD4VZ7ELPKW7DHC3BWBSD2GCZ7IUQQXZIORRBU",
+               "SCAMLK2ZNL35WSMW37E7UD4VZ7ELPKW7DHC3BWBSD2GCZ7IUQQXZIORRBU",
+               "SOAMLK2ZNL35WSMW37E7UD4VZ7ELPKW7DHC3BWBSD2GCZ7IUQQXZIORRBU",
+               "SUAMLK2ZNL35WSMW37E7UD4VZ7ELPKW7DHC3BWBSD2GCZ7IUQQXZIORRBU"
+              ]
+      Enum.each(seeds, fn seed ->
+        assert {:ok, _nkey} = NKEYS.from_seed(seed)
+      end)
+
+      invalid_seeds = [
+                       "SDAMLK2ZNL35WSMW37E7UD4VZ7ELPKW7DHC3BWBSD2GCZ7IUQQXZIORRBU",
+                       "SBAMLK2ZNL35WSMW37E7UD4VZ7ELPKW7DHC3BWBSD2GCZ7IUQQXZIORRBU"
+                      ]
+      Enum.each(invalid_seeds, fn seed ->
+        assert {:error, :invalid_seed} = NKEYS.from_seed(seed)
+      end)
+
+      invalid_seeds = [
+                       "PWAMLK2ZNL35WSMW37E7UD4VZ7ELPKW7DHC3BWBSD2GCZ7IUQQXZIORRBU",
+                       "PMAMLK2ZNL35WSMW37E7UD4VZ7ELPKW7DHC3BWBSD2GCZ7IUQQXZIORRBU"
+                      ]
+      Enum.each(invalid_seeds, fn seed ->
+        assert {:error, :invalid_seed} = NKEYS.from_seed(seed)
+      end)
+    end
   end
 
-  test "a second nonce" do
-    nonce = "iBFByN3zQjAT7dQ"
-    {:ok, nkeys} = NKEYS.from_seed(@seed)
-    signed_nonce = NKEYS.sign(nkeys, nonce)
-    encoded_signed_nonce = Base.url_encode64(signed_nonce)
+  describe "sign/2" do
+    @seed "SUAMLK2ZNL35WSMW37E7UD4VZ7ELPKW7DHC3BWBSD2GCZ7IUQQXZIORRBU"
 
-    assert encoded_signed_nonce ==
-             "kagPGrixaWS5yuHqw9nTQrda1Q376fK3fRCGtYdF4_w2aSk-4O7Ca0JM0qvzm69HH6MoMps2yF6Q0Qs830JZCA=="
+    test "from_seed" do
+      nonce = "PXoWU7zWAMt75FY"
+      {:ok, nkeys} = NKEYS.from_seed(@seed)
+      signed_nonce = NKEYS.sign(nkeys, nonce)
+      encoded_signed_nonce = Base.encode64(signed_nonce)
+
+      assert encoded_signed_nonce ==
+               "ZaAiVDgB5CeYoXoQ7cBCmq+ZllzUnGUoDVb8C7PilWvCs8XKfUchAUhz2P4BYAF++Dg3w05CqyQFRDiGL6LrDw=="
+    end
+
+    test "a second nonce" do
+      nonce = "iBFByN3zQjAT7dQ"
+      {:ok, nkeys} = NKEYS.from_seed(@seed)
+      signed_nonce = NKEYS.sign(nkeys, nonce)
+      encoded_signed_nonce = Base.url_encode64(signed_nonce)
+
+      assert encoded_signed_nonce ==
+               "kagPGrixaWS5yuHqw9nTQrda1Q376fK3fRCGtYdF4_w2aSk-4O7Ca0JM0qvzm69HH6MoMps2yF6Q0Qs830JZCA=="
+    end
   end
 
   test "creating a public nkey" do
